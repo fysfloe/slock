30f036d3befdb57a7533fff34dff38083fac42cf
Florian Csizmazia <florian.csizmazia@gugler.at>
2017-01-08 11:38:21 +0100

    w/o angular

--- app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sl-app',
-  templateUrl: 'app/app.component.html',
-  // styleUrls: ['app/app.component.css']
-})
-export class AppComponent { }
